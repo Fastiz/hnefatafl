371a368561e2a1d3f992176a27b602a10ab8d470
fastiz <facuastiz@gmail.com>
2021-02-04 15:04:26 -0300

    cors

--- express/src/main.ts
+++ express/src/main.ts
@@ -6,7 +6,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: "http:/64.227.1.4:3000",
+        origin: "http://64.227.1.4",
     }
 });
 
